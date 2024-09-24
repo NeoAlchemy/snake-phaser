1248a2d03b25e418f29767f75eee1d9733c379c2
NeoAlchemy <neoalchemy@gmail.com>
2024-09-24 17:03:41 -0500

    handling desktop versus mobile

--- src/game/main.ts
+++ src/game/main.ts
@@ -19,7 +19,7 @@ const config: Phaser.Types.Core.GameConfig = {
         },
     },
     scale: {
-        mode: Phaser.Scale.FIT
+        mode: Phaser.Scale.NONE
     },
     scene: [
         Boot,
